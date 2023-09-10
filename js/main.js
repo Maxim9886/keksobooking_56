142a6977b5de3d30f0c8a52958ae564eb875e4bb
GUNMAXbit <gunmax9696@gmail.com>
2023-09-10 20:13:42 +0500

    build

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import './map.js';
-import './slider.js';
-import './form.js';
-import './Photos.js';
-import {getData} from './api.js';
-import {setUserFormSubmit} from './form.js';
-
-getData();
-setUserFormSubmit();
